e260c3cb1b05fc57100b152ebb01f0fe408e4cff
SujitSarkar <sujitsarkar.cse@gmail.com>
2025-10-04 16:41:58 +0600

    fix: remove hard-coded string

--- lib/presentation/analytics/pages/analytics_page.dart
+++ lib/presentation/analytics/pages/analytics_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:take_home/core/constants/app_strings.dart';
 
 class AnalyticsPage extends StatelessWidget {
   const AnalyticsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Analytics')));
+    return const Scaffold(body: Center(child: Text(AppStrings.analyticsPage)));
   }
 }
--- lib/presentation/dashboard/pages/dashboard_page.dart
+++ lib/presentation/dashboard/pages/dashboard_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:take_home/core/constants/app_strings.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Dashboard')));
+    return const Scaffold(body: Center(child: Text(AppStrings.dashboard)));
   }
 }
