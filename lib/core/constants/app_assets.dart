e70ff749b33a175eb3520d2323699d7719f1aedd
SujitSarkar <sujitsarkar.cse@gmail.com>
2025-10-05 21:28:23 +0600

    feat: implement Dashboard header bg image

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesHeader
+  /// assets/images/header.jpg
+  static const String assetsImagesHeader = "assets/images/header.jpg";
+}
+
